ee5cdcaec142b37c271cac28286f95c50d8b9515
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-05-15 17:23:56 +0000

    i don't know what I'm doing anymore

--- app/src/main/java/application/TccApplication.java
+++ app/src/main/java/application/TccApplication.java
@@ -2,12 +2,20 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class TccApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TccApplication.class, args);
+	}
+
+	@Bean(name="entityManagerFactory")
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		return sessionFactory;
 	}
 
 }
--- app/src/main/java/application/animal/AnimalResponseDTO.java
+++ app/src/main/java/application/animal/AnimalResponseDTO.java
@@ -1,5 +1,4 @@
 package application.animal;
-import application.animal.Animal;
 
 public record AnimalResponseDTO(Long id, String name, Integer age, String gender, String breed, String coatColor, String coatLength, String situation, String behavior, String observation) {
     public AnimalResponseDTO(Animal animal) {
