51e9e3851044d07b9a9da16a227ff9e52d0f0c4d
Henrique Romero <luishenriquebuenoromero@gmail.com>
2024-06-07 12:06:41 +0000

    FIX ERROR 🤩✨

--- app/src/main/java/application/TccApplication.java
+++ app/src/main/java/application/TccApplication.java
@@ -2,20 +2,12 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class TccApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TccApplication.class, args);
-	}
-
-	@Bean(name="entityManagerFactory")
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		return sessionFactory;
 	}
 
 }
